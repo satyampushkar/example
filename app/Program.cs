4064e62eadca4ce2854276dd98abe9d06cca0d97
satyampushkar <42037502+satyampushkar@users.noreply.github.com>
2021-05-22 19:57:37 +0530

    Initial commit

---
+++ app/Program.cs
@@ -0,0 +1,16 @@
+﻿using library;
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var start = new Start();
+
+            Console.WriteLine(string.Format("Hello World! {0}", start.Multiply(2, 2)));
+        }
+    }
+}
---
+++ library/Start.cs
@@ -0,0 +1,20 @@
+﻿namespace library
+{
+    public class Start
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
